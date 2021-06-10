978b5e2d2ecf4b0a39622c0a0ae1e97957897d04
aaryan11-hash <aaryan2000dpspune@gmail.com>
2021-06-10 11:47:21 +0530

    first commit,preliminary project files and,buisines logic and code base.

---
+++ mssc-beer-service/target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-06-10T01:09:25+0530",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.9 (GraalVM Community)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
