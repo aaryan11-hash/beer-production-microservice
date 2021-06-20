b90754c1bf39c0d3d4d710ae47abc5f196fae17a
aaryan11-hash <aaryan2000dpspune@gmail.com>
2021-06-20 10:45:43 +0530

    added spring cloud config service

---
+++ mssc-config-server/src/main/java/guru/sfg/brewery/configserver/MsscConfigServerApplication.java
@@ -0,0 +1,17 @@
+package guru.sfg.brewery.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class MsscConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscConfigServerApplication.class, args);
+	}
+
+}
