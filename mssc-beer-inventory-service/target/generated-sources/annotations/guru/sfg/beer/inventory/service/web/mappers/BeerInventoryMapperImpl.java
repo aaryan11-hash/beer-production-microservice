410fb6bf1e23095d636fe946e89859dd687fb952
aaryan11-hash <aaryan2000dpspune@gmail.com>
2021-06-22 17:47:27 +0530

    added docker file and configuration for inventory,order and berr-service applications

--- mssc-beer-inventory-service/target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ mssc-beer-inventory-service/target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-20T15:41:50+0530",
+    date = "2021-06-22T17:40:33+0530",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.9 (GraalVM Community)"
 )
 @Component
--- mssc-beer-service/target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ mssc-beer-service/target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-20T15:17:11+0530",
+    date = "2021-06-21T17:41:24+0530",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.9 (GraalVM Community)"
 )
 @Component
